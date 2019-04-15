1854f007aa51a3a45d013c49119f3d102a3f58f9
Cody-Price <ctprice11@gmail.com>
2019-04-15 11:02:49 -0600

    Initialize Note

---
+++ src/Components/Note/Note.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Note extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <div className="note">
+        I am a note
+      </div>
+    )
+  }
+}
+
+export default Note
